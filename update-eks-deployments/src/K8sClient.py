ee85022cea6a8d8e19ffb5171a5d939ebb144261
Patryk Bednarz <patrykbednarz8@gmail.com>
2023-08-10 22:47:32 +0200

    added K8sClient more methods for managing deployments

--- update-eks-deployments/src/K8sClient.py
+++ update-eks-deployments/src/K8sClient.py
@@ -17,22 +17,43 @@ class K8sClient:
         self.api_client = client.ApiClient()
         self.apps_v1_api = client.AppsV1Api(self.api_client)
 
-    def create_deployments(self, deployments_body: List[V1Deployment], namespace: str = "default"):
-        for deployment in deployments_body:
-            print(f"Creating deployment: {deployment.metadata.name}")
-            self.apps_v1_api.create_namespaced_deployment(body=deployment, namespace=namespace)
+    def create_single_deployment(self, deployment: V1Deployment, namespace: str = "default"):
+        print(f"Creating deployment: {deployment.metadata.name}")
+        self.apps_v1_api.create_namespaced_deployment(body=deployment, namespace=namespace)
+
+    def create_deployments(self, deployments: List[V1Deployment], namespace: str = "default"):
+        for deployment in deployments:
+            self.create_single_deployment(deployment=deployment, namespace=namespace)
+
+    def create_only_not_existing_deployments(self, deployments: List[V1Deployment], namespace: str = "default"):
+        existing_deployments_names = self.get_deployments_names(namespace)
+        not_existing_deployments = [
+            deployment_body
+            for deployment_body in deployments
+            if deployment_body.metadata.name not in existing_deployments_names
+        ]
+        self.create_deployments(not_existing_deployments)
 
     def delete_all_deployments(self, namespace: str = "default"):
-        deployments = self.apps_v1_api.list_namespaced_deployment(namespace=namespace)
-        for deployment in deployments.items:
-            deployment_name = deployment.metadata.name
-            print(f"Deleting deployment: {deployment_name}")
+        deployments_names = self.get_deployments_names(namespace)
+        for deployment_name in deployments_names:
 
             # Delete the deployment
-            self.apps_v1_api.delete_namespaced_deployment(
+            self.delete_deployment(
                 name=deployment_name,
                 namespace=namespace
             )
+
+    def delete_deployment(self, name: str, namespace: str = "default"):
+        print(f"Deleting deployment: {name}")
+        self.apps_v1_api.delete_namespaced_deployment(
+            name=name,
+            namespace=namespace
+        )
+
+    def get_deployments_names(self, namespace: str = "default") -> List[str]:
+        deployments = self.apps_v1_api.list_namespaced_deployment(namespace=namespace)
+        return [deployment.metadata.name for deployment in deployments.items]
 
     def _get_cube_config(self, cluster: Dict[str, str], bearer_token: str) -> Dict:
         return {
--- update-eks-deployments/src/update_eks_deployments.py
+++ update-eks-deployments/src/update_eks_deployments.py
@@ -14,7 +14,9 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     "Lambda handler"
-    streams = ["stream-4", "stream-5", "stream-6"]
+    streams = ["stream-1", "stream-2", "stream-3"]
+    streams_newly_added = ["stream-1", "stream-2", "stream-3", "stream-4"]
+    stream_to_delete = "stream-1"
 
     cluster_name = "fargate-cluster"
     session = boto3.session.Session()
@@ -26,7 +28,16 @@ def handler(event, context):
                    for steam_name in streams]
 
     k8s_client.create_deployments(deployments)
+
+    newly_added_deployments = [HardcodedDeploymentTemplateProvider.create_deployment_template(name=steam_name)
+                   for steam_name in streams_newly_added]
+
+    k8s_client.create_only_not_existing_deployments(newly_added_deployments)
+
+    k8s_client.delete_deployment(stream_to_delete)
+
     k8s_client.delete_all_deployments()
+
 
 
 print(handler(None, None))
